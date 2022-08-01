5a756e434ccbc858cdd2770fdf60f794d771c0be
David Lemvigh <dle@siteimprove.com>
2022-08-01 14:40:15 +0200

    Handle slugify url case-sensitive

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # slugify-action
 
 Github action to slugify values
+
+
+Heavily inspired by [slugify-value](https://github.com/rlespinasse/slugify-value/blob/v1.x/slugify.sh)
--- slugify.sh
+++ slugify.sh
@@ -2,13 +2,38 @@
 
 echo "Slugify value: $VALUE"
 
-SLUG=${VALUE}
+slug() {
+  # 1st : Remove refs prefix
+  # 2d : Replace unwanted characters
+  # 3d : Remove leading hypens
+  output=$(sed -E 's#refs/[^\/]*/##;s/[^a-zA-Z0-9._-]+/-/g;s/^-*//' <<<"$1")
+  reduce "$output"
+}
 
-if [ ${#VALUE} -ge $MAXLENGTH ]; then
-	HASH=$(echo $VALUE | md5sum)
-	SLUG=${VALUE:0:($MAXLENGTH - 7)}-${HASH:0:6}
-fi
+slug_url() {
+  # 1st : Remove refs prefix
+  # 2d : Replace unwanted characters
+  # 3d : Remove leading hypens
+  output=$(sed -E 's#refs/[^\/]*/##;s/[^a-zA-Z0-9-]+/-/g;s/^-*//' <<<"$1")
+  reduce "$output"
+}
 
-echo "Slugified value: $SLUG"
+reduce() {
+  reduced_value="$1"
+  if [ "${MAXLENGTH}" != "nolimit" ]; then
+    reduced_value=$(cut -c1-"${MAXLENGTH}" <<<"$reduced_value")
+  fi
+  # 1st : Remove trailing hypens
+  sed -E 's/-*$//' <<<"$reduced_value"
+}
 
-echo "::set-output name=slug::${SLUG,,}"
+SLUG=$(slug "${VALUE,,}")
+SLUG_CS=$(slug "$VALUE")
+SLUG_URL=$(slug_url "${VALUE,,}")
+SLUG_URL_CS=$(slug_url "$CS_VALUE")
+
+echo "::set-output name=value::${SLUG}"
+echo "::set-output name=slug::${SLUG}"
+echo "::set-output name=slug-cs::${SLUG_CS}"
+echo "::set-output name=slug-url::${SLUG_URL}"
+echo "::set-output name=slug-url-cs::${SLUG_URL_CS}"
